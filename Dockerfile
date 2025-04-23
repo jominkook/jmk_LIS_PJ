9ffb99835fce785d90ce19a2d518f37b2f5a763c
jominkook <whaksenaos@naver.com>
2025-04-23 21:47:43 +0900

    oracle DB -> mysql DB 변경

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+# 1. OpenJDK 이미지를 기반으로 사용
+FROM openjdk:17-jdk-slim
+
+# 2. 작업 디렉토리 설정
+WORKDIR /app
+
+# 3. JAR 파일 복사
+COPY target/logistic-0.0.1-SNAPSHOT.jar app.jar
+
+# 4. 애플리케이션 실행 포트 설정
+EXPOSE 8080
+
+# 5. 애플리케이션 실행 명령
+ENTRYPOINT ["java", "-jar", "app.jar"]
--- src/main/java/com/yk/logistic/DataInitializer.java
+++ src/main/java/com/yk/logistic/DataInitializer.java
@@ -1,60 +1,60 @@
-package com.yk.logistic;
-
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.stereotype.Component;
-
-import com.yk.logistic.domain.address.Address;
-import com.yk.logistic.domain.category.Category;
-import com.yk.logistic.domain.member.Member;
-import com.yk.logistic.domain.member.MemberRole;
-import com.yk.logistic.repository.category.CategoryRepository;
-import com.yk.logistic.repository.member.MemberRepository;
-
-import lombok.RequiredArgsConstructor;
-
-@RequiredArgsConstructor
-@Component
-public class DataInitializer implements CommandLineRunner {
-
-    private final MemberRepository memberRepository;
-    private final CategoryRepository categoryRepository;
-    //private final StockRepository stockRepository;
-    private final BCryptPasswordEncoder bCryptPasswordEncoder;
-
-    @Override
-    public void run(String... args) throws Exception {
-        // 샘플 멤버 데이터 생성
-        Member producer = Member.builder()
-                .email("jominkook@example.com")
-                .password(bCryptPasswordEncoder.encode("password1234"))
-                .name("조민국")
-                .role(MemberRole.SELLER)
-                .address(new Address("서울특별시 종로구 세종대로 110", "서울특별시", "03172"))
-                .build();
-        memberRepository.save(producer);
-
-        Member seller = Member.builder()
-                .email("jominsoo@example.com")
-                .password(bCryptPasswordEncoder.encode("password1234"))
-                .name("조민수")
-                .role(MemberRole.SELLER)
-                .address(new Address("서울특별시 강남구 테헤란로 212", "서울특별시", "06221"))
-                .build();
-        memberRepository.save(seller);
-
-        // 샘플 카테고리 데이터 생성
-        Category electronics = new Category("전자제품", null);
-        categoryRepository.save(electronics);
-
-        Category computers = new Category("컴퓨터", electronics);
-        categoryRepository.save(computers);
-
-        Category smartphones = new Category("스마트폰", electronics);
-        categoryRepository.save(smartphones);
-
-        Category furniture = new Category("가구", null);
-        categoryRepository.save(furniture);
-
-    }
-}
+//package com.yk.logistic;
+//
+//import org.springframework.boot.CommandLineRunner;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.stereotype.Component;
+//
+//import com.yk.logistic.domain.address.Address;
+//import com.yk.logistic.domain.category.Category;
+//import com.yk.logistic.domain.member.Member;
+//import com.yk.logistic.domain.member.MemberRole;
+//import com.yk.logistic.repository.category.CategoryRepository;
+//import com.yk.logistic.repository.member.MemberRepository;
+//
+//import lombok.RequiredArgsConstructor;
+//
+//@RequiredArgsConstructor
+//@Component
+//public class DataInitializer implements CommandLineRunner {
+//
+//    private final MemberRepository memberRepository;
+//    private final CategoryRepository categoryRepository;
+//    //private final StockRepository stockRepository;
+//    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+//
+//    @Override
+//    public void run(String... args) throws Exception {
+//        // 샘플 멤버 데이터 생성
+//        Member producer = Member.builder()
+//                .email("jominkook@example.com")
+//                .password(bCryptPasswordEncoder.encode("password1234"))
+//                .name("조민국")
+//                .role(MemberRole.SELLER)
+//                .address(new Address("서울특별시 종로구 세종대로 110", "서울특별시", "03172"))
+//                .build();
+//        memberRepository.save(producer);
+//
+//        Member seller = Member.builder()
+//                .email("jominsoo@example.com")
+//                .password(bCryptPasswordEncoder.encode("password1234"))
+//                .name("조민수")
+//                .role(MemberRole.SELLER)
+//                .address(new Address("서울특별시 강남구 테헤란로 212", "서울특별시", "06221"))
+//                .build();
+//        memberRepository.save(seller);
+//
+//        // 샘플 카테고리 데이터 생성
+//        Category electronics = new Category("전자제품", null);
+//        categoryRepository.save(electronics);
+//
+//        Category computers = new Category("컴퓨터", electronics);
+//        categoryRepository.save(computers);
+//
+//        Category smartphones = new Category("스마트폰", electronics);
+//        categoryRepository.save(smartphones);
+//
+//        Category furniture = new Category("가구", null);
+//        categoryRepository.save(furniture);
+//
+//    }
+//}

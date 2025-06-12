3449488fc12b0b54d261b202fb2fc7b5ed59c23b
jominkook <whaksenaos@naver.com>
2025-06-12 23:13:45 +0900

    [Modified] 개발환경수정 및 로그인 처리기능 수정

--- src/main/java/com/yk/logistic/controller/member/MemberApiController.java
+++ src/main/java/com/yk/logistic/controller/member/MemberApiController.java
@@ -1,12 +1,12 @@
 package com.yk.logistic.controller.member;
 
+import com.yk.logistic.constant.SessionConst;
+import com.yk.logistic.domain.member.Member;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.yk.logistic.dto.member.AddMemberRequest;
 import com.yk.logistic.service.member.MemberService;
@@ -34,5 +34,18 @@ public class MemberApiController {
             new SecurityContextLogoutHandler().logout(request, response, auth);
         }
         return "redirect:/login";
+    }
+
+    //동일한 login 매핑을 테스트로 작성해둔곳이 있다면 반드시 에러가난다
+    @PostMapping("/login")
+    public String login(@RequestParam String useremail, @RequestParam String password, HttpSession session) {
+        Member member = memberService.login(useremail, password);
+        if (member != null) {
+            session.setAttribute(SessionConst.LOGIN_ID, member.getId());
+            return "/items";
+        } else {
+        	System.out.println("Login failed for useremail: " + useremail);
+            return "redirect:/login?error";
+        }
     }
 }
--- src/main/java/com/yk/logistic/controller/member/MemberViewController.java
+++ src/main/java/com/yk/logistic/controller/member/MemberViewController.java
@@ -2,9 +2,11 @@ package com.yk.logistic.controller.member;
 
 
 
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @Controller
 @RequestMapping("/view")
